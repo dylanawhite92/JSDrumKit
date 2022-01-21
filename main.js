22662a09ff22ed00ce2ea04540e2ece5a6413e5b
The Crystal King <dylanawhite11@gmail.com>
2022-01-21 16:03:40 -0600

    playing class properly appending to div, style applies on key press

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 window.addEventListener('keydown', e => {
     const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+    const key = document.querySelector(`.key[data-key="${e.key}"]`);
+    
     if(!audio) return; // Stop function if no value returned
     audio.currentTime = 0; // Rewind sound when key pressed
     audio.play();
+    
+    key.classList.add('playing');
 });
