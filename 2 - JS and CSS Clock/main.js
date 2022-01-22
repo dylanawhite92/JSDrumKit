9ed3d8882e52d8d601061eaed09cd2fdf5b17a75
The Crystal King <dylanawhite11@gmail.com>
2022-01-22 14:45:02 -0600

    properly rotating clock hand around face in correspondence with real time seconds

--- 2 - JS and CSS Clock/main.js
+++ 2 - JS and CSS Clock/main.js
@@ -1,6 +1,12 @@
+const secondHand = document.querySelector('.second-hand');
+
 setDate = () => {
     const now = new Date();
     const seconds = now.getSeconds();
+
+    // convert to degrees to properly rotate around clock face
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform  = `rotate(${secondsDegrees}deg)`;
     console.log(seconds);
 }
 
