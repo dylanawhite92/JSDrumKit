25369abfa516acdc5210cc0d09984b21837ca4af
The Crystal King <dylanawhite11@gmail.com>
2022-01-22 15:05:56 -0600

    clock properly rendering on screen, working on case to prevent animation break when clock turns over

--- 2 - JS and CSS Clock/main.js
+++ 2 - JS and CSS Clock/main.js
@@ -1,13 +1,23 @@
 const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 setDate = () => {
     const now = new Date();
     const seconds = now.getSeconds();
+    const mins = now.getMinutes();
+    const hour = now.getHours();
 
     // convert to degrees to properly rotate around clock face
     const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const minDegrees = ((mins / 60) * 360) + 90;
+    const hourDegrees = ((hour / 12) * 360) + 90;
+
     secondHand.style.transform  = `rotate(${secondsDegrees}deg)`;
-    console.log(seconds);
+    minHand.style.transform = `rotate(${minDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+   
+    console.log(`${hour}:${mins}:${seconds}`);
 }
 
 setInterval(setDate, 1000);
