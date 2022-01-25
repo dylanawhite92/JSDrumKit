08b4f33e1a929ed692dc212b69d39f05d7b2bf6e
The Crystal King <dylanawhite11@gmail.com>
2022-01-25 12:14:41 -0600

    realized code could be simplified, refactored to be in-line

--- 4 - Array Cardio Day 1/main.js
+++ 4 - Array Cardio Day 1/main.js
@@ -23,11 +23,7 @@ const inventors = [
 
     // Array.prototype.filter()
     // 1. Filter the list of inventors for those who were born in the 1500's
-    const fifteen = inventors.filter((inventor) => {
-        if(inventor.year >= 1500 && inventor.year < 1600) {
-            return true;
-        }
-    });
+    const fifteen = inventors.filter(inventor => inventor.year >= 1500 && inventor.year < 1600);
 
     // table makes viewing arrays in console much neater
     console.table(fifteen);
