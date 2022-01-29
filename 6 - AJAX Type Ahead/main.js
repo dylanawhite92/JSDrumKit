9695e6eba819640311f0f5de3dae637571b29945
The Crystal King <dylanawhite11@gmail.com>
2022-01-28 19:19:44 -0600

    fetching json endpoint and spreading it into cities array

--- 6 - AJAX Type Ahead/main.js
+++ 6 - AJAX Type Ahead/main.js
@@ -1,4 +1,7 @@
 // A cities.json file from github
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
-
 const cities = [];
+// Utilizing the browser's built-in fetch to return our promise
+fetch(endpoint)
+    .then(blob => blob.json()
+    .then(data => cities.push(...data)));
