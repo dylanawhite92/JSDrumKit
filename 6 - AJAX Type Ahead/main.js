a0dc61de6747e29f89ba60001e5c4e32f35daac9
The Crystal King <dylanawhite11@gmail.com>
2022-01-28 19:27:30 -0600

    added findMatches function using regex to filter through array for matches, tested in browser console

--- 6 - AJAX Type Ahead/main.js
+++ 6 - AJAX Type Ahead/main.js
@@ -5,3 +5,12 @@ const cities = [];
 fetch(endpoint)
     .then(blob => blob.json()
     .then(data => cities.push(...data)));
+
+function findMatches(wordToMatch, cities) {
+    return cities.filter(place => {
+        // Determine if city/state matches what is searched
+        const regex = new RegExp(wordToMatch, 'gi');
+        
+        return place.city.match(regex) || place.state.match(regex);
+    })
+}
