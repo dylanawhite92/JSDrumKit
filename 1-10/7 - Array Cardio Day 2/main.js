38bf0cbacf25dcd81e2a24f4d74cd10be162a731
The Crystal King <dylanawhite11@gmail.com>
2022-02-07 11:04:31 -0600

    modified main.js

--- 1-10/7 - Array Cardio Day 2/main.js
+++ 1-10/7 - Array Cardio Day 2/main.js
@@ -33,12 +33,15 @@ const comments = [
     // 2. Array.prototype.find()
     // Find is like filter, but instead returns just the one you are looking for
     // find the comment with the ID of 823423
+    
+    // First go
     // const comment = comments.find(comment => {
     //     if (comment.id === 823423) {
     //         return true;
     //     }
     // });
 
+    // Single line, implicit return
     const comment = comments.find(comment => comment.id === 823423);
 
     console.log(comment);
