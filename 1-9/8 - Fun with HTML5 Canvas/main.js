8a509a4083a85f9819a664d5ce4a95c43f12b9b9
The Crystal King <dylanawhite11@gmail.com>
2022-02-07 12:08:23 -0600

    able to draw separate lines whose origin moves with the mouse, instead of a connected line from the origin point of the window

--- 1-9/8 - Fun with HTML5 Canvas/main.js
+++ 1-9/8 - Fun with HTML5 Canvas/main.js
@@ -19,12 +19,20 @@ function draw(e) {
     // Stop the function from running when not holding mouse down
     if (!isDrawing) return;
     ctx.beginPath();
+    // Start from
     ctx.moveTo(lastX, lastY);
+    // Go to
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
+
+    [lastX, lastY] = [e.offsetX, e.offsetY];
 }
 
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', () => isDrawing = true);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
