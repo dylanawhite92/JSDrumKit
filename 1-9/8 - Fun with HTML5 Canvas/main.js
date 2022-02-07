dc86dc99a310be308c05a2d83200e34adaac96d7
The Crystal King <dylanawhite11@gmail.com>
2022-02-07 11:50:10 -0600

    capturing events for mousedown, mouseup, mousemove, and mouseout cases

--- 1-9/8 - Fun with HTML5 Canvas/main.js
+++ 1-9/8 - Fun with HTML5 Canvas/main.js
@@ -14,3 +14,13 @@ ctx.lineCap = 'round';
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+
+function draw(e) {
+    if (!isDrawing) return; // Stop the function from running when not holding mouse down"
+    console.log(e);
+}
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
