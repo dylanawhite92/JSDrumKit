0c32891720814fa76ca7614c2b82aa9c274495e2
The Crystal King <dylanawhite11@gmail.com>
2022-02-07 11:03:27 -0600

    second problem done, did it two ways with the second being a single line of code with implicit return

--- 1-10/7 - Array Cardio Day 2/main.js
+++ 1-10/7 - Array Cardio Day 2/main.js
@@ -33,6 +33,15 @@ const comments = [
     // 2. Array.prototype.find()
     // Find is like filter, but instead returns just the one you are looking for
     // find the comment with the ID of 823423
+    // const comment = comments.find(comment => {
+    //     if (comment.id === 823423) {
+    //         return true;
+    //     }
+    // });
+
+    const comment = comments.find(comment => comment.id === 823423);
+
+    console.log(comment);
 
     // 3. Array.prototype.findIndex()
     // Find the comment with this ID
