9537134ee87cc7e1db1ee27924265f6b5bbd631e
The Crystal King <dylanawhite11@gmail.com>
2022-02-07 11:39:25 -0600

    grabbing context of canvas element, resizing based on size of window

---
+++ 1-9/8 - Fun with HTML5 Canvas/main.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector('#draw');
+
+// grab context of canvas, define 2d or 3d
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
