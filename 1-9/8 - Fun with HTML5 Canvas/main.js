aca6ff165553bca2422486991213c945a2238c21
The Crystal King <dylanawhite11@gmail.com>
2022-02-07 11:45:42 -0600

    setting line styles, starting variables

--- 1-9/8 - Fun with HTML5 Canvas/main.js
+++ 1-9/8 - Fun with HTML5 Canvas/main.js
@@ -1,7 +1,16 @@
 const canvas = document.querySelector('#draw');
 
 // grab context of canvas, define 2d or 3d
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+// Set color, property for when two lines meet, and end of line shape"
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
