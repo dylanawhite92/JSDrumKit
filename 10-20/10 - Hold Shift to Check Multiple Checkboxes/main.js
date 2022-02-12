12e5c007b50054952196f150fe982e3914d73f58
The Crystal King <dylanawhite11@gmail.com>
2022-02-11 19:08:17 -0600

    able to loop over boxes and display range between the first check and last checked if holding shift

--- 10-20/10 - Hold Shift to Check Multiple Checkboxes/main.js
+++ 10-20/10 - Hold Shift to Check Multiple Checkboxes/main.js
@@ -1,7 +1,23 @@
 const checkboxes = document.querySelectorAll(".inbox input[type=checkbox]");
 
+let lastChecked;
+
 function handleCheck(e) {
-    console.log(e);
+    // Check if shift key is down
+    // AND check that they are checking it (not unchecking)
+    let inBetween = false;
+
+    if (e.shiftKey && this.checked) {
+        checkboxes.forEach(checkbox => {
+            console.log(checkbox);
+            if (checkbox === this || checkbox === lastChecked) {
+                inBetween === !inBetween;
+                console.log("Starting to check them inbetween!");
+            }
+        })
+    }
+    
+    lastChecked = this;
 } 
 
 checkboxes.forEach(checkbox => checkbox.addEventListener("click", handleCheck));
