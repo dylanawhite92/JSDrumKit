754859417493acdfb40db92e76edef5d362ab0d4
The Crystal King <dylanawhite11@gmail.com>
2022-02-15 17:26:03 -0600

    grabbing data-skip attribute and applying it to current video time

--- 10-20/11 - Custom HTML5 Video Player/main.js
+++ 10-20/11 - Custom HTML5 Video Player/main.js
@@ -4,7 +4,7 @@ const video = player.querySelector(".viewer");
 const progress = player.querySelector(".progress");
 const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
-const skipButtons = player.querySelector("[data-skip]");
+const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelector(".player__slider");
 
 // Build out functions
@@ -19,11 +19,14 @@ function updateButton() {
 }
 
 function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
 
 }
 
 // Hook up event listeners
 video.addEventListener("click", togglePlay);
-toggle.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
+
+toggle.addEventListener("click", togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
