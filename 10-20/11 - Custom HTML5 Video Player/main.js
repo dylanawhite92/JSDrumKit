8d98eb0266408fb5e78ed20939ccc9cad5268c35
The Crystal King <dylanawhite11@gmail.com>
2022-02-15 17:47:05 -0600

    functionality to change video speed between 0.5x and 2x, and updating slider text so that the viewer can see their own speed

--- 10-20/11 - Custom HTML5 Video Player/main.js
+++ 10-20/11 - Custom HTML5 Video Player/main.js
@@ -5,7 +5,8 @@ const progress = player.querySelector(".progress");
 const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
-const ranges = player.querySelector(".player__slider");
+const ranges = player.querySelectorAll(".player__slider");
+let speed = player.querySelector("#player__speed");
 
 // Build out functions
 function togglePlay() {
@@ -23,6 +24,11 @@ function skip() {
 
 }
 
+function handleRangeUpdate(e) {
+    video[this.name] = this.value;
+    speed.innerHTML = `Speed: ${this.value}x`;
+}
+
 // Hook up event listeners
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
@@ -30,3 +36,5 @@ video.addEventListener("pause", updateButton);
 
 toggle.addEventListener("click", togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate));
