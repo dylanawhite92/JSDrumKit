54f6dea9259300a922223dcbab883347b57066ca
The Crystal King <dylanawhite11@gmail.com>
2022-02-16 00:33:28 -0600

    imported debouncer function to limit propagation and improve performance

---
+++ 10-20/13 - Slide in on Scroll/main.js
@@ -0,0 +1,28 @@
+const sliderImages = document.querySelectorAll(".slide-in");
+
+// Prevent checkSlide from running 100 times a second
+function debounce(func, wait = 20, immediate = true) {
+	var timeout;
+
+	return function() {
+		var context = this, args = arguments;
+
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+
+		var callNow = immediate && !timeout;
+
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+        
+		if (callNow) func.apply(context, args);
+	};
+};
+
+function checkSlide(e) {
+    console.count(e);
+}
+
+// window.addEventListener("scroll", checkSlide);
