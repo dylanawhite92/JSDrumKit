7865f1853c086ea042ccb573445d4b3c1300d232
The Crystal King <dylanawhite11@gmail.com>
2022-02-15 18:01:41 -0600

    able to scrub on click and update progress bar, drag and drop is next

--- 10-20/11 - Custom HTML5 Video Player/main.js
+++ 10-20/11 - Custom HTML5 Video Player/main.js
@@ -35,6 +35,11 @@ function handleProgress() {
     progressBar.style.flexBasis = `${percent}%`;
 }
 
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
 // Hook up event listeners
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
@@ -45,3 +50,5 @@ toggle.addEventListener("click", togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
 ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate));
+
+progress.addEventListener("click", scrub);
