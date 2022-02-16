533fee754b1cda92b92895b91b95ed8d8dcdfc5f
The Crystal King <dylanawhite11@gmail.com>
2022-02-15 21:23:50 -0600

    when secret code is detected, run cornify function

--- 10-20/12 - Key Sequence Detection (KONAMI CODE)/main.js
+++ 10-20/12 - Key Sequence Detection (KONAMI CODE)/main.js
@@ -5,4 +5,8 @@ window.addEventListener("keyup", (e) => {
     pressed.push(e.key);
     pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
     console.log(pressed);
+
+    if(pressed.join("").includes(secretCode)) {
+        cornify_add();
+    }
 })
