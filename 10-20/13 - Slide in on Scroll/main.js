3a9d2ac1d491ff81d4cc6bc99dc4df6a83def13b
The Crystal King <dylanawhite11@gmail.com>
2022-02-16 00:55:28 -0600

    slide in on scroll functionality working

--- 10-20/13 - Slide in on Scroll/main.js
+++ 10-20/13 - Slide in on Scroll/main.js
@@ -1,28 +1,38 @@
-const sliderImages = document.querySelectorAll(".slide-in");
+const slideImages = document.querySelectorAll(".slide-in");
 
 // Prevent checkSlide from running 100 times a second
-function debounce(func, wait = 40, immediate = true) {
-	var timeout;
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
 
-	return function() {
-		var context = this, args = arguments;
+function checkSlide() {
+    // Determine when a picture is peeking out into the window 
+    // and would be partially revealed before animating it onto the screen
+    slideImages.forEach(slideImage => {
+        const slideInAt = (window.scrollY + window.innerHeight) - 
+        slideImage.height / 2;
+        
+        const imageBottom = slideImage.offsetTop + slideImage.height;
+        const isHalfShown = slideInAt > slideImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
 
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-
-		var callNow = immediate && !timeout;
-
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-
-		if (callNow) func.apply(context, args);
-	};
-};
-
-function checkSlide(e) {
-    console.count(e);
+        if (isHalfShown && isNotScrolledPast) {
+            slideImage.classList.add("active");
+        } else {
+            slideImage.classList.remove("active");
+        }
+    });
 }
 
 window.addEventListener("scroll", debounce(checkSlide));
